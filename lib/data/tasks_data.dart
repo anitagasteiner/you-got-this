b98e043b4edb41ffd7adfe49d2dc48cadc8ddefe
anitagasteiner <anita.gasteiner@posteo.de>
2026-03-19 09:52:48 +0100

    variable 'category' added to task model, tasks data: added test data

--- lib/data/tasks_data.dart
+++ lib/data/tasks_data.dart
@@ -1,6 +1,9 @@
 import '../models/task_model.dart';
 
 final Map<String, TaskModel> tasks = {
-  'task1': TaskModel(name: 'Bettwäsche wechseln', recurrence: 14),
-  'task2': TaskModel(name: 'Handtücher wechseln', recurrence: 4),
+  'task1': TaskModel(name: 'Bettwäsche wechseln', recurrence: 14, category: 'toDo'),
+  'task2': TaskModel(name: 'Handtücher wechseln', recurrence: 4, category: 'toDoSoon'),
+  'task3': TaskModel(name: 'Zahnbürsten austauschen', recurrence: 60, category: 'stillFine'),
+  'task4': TaskModel(name: 'Fenster putzen', recurrence: 365, category: 'doneRecently'),
+  'task5': TaskModel(name: 'Wohnzimmer abstauben', recurrence: 14, category: 'done'),
 };
--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,6 +1,7 @@
 class TaskModel {
   final String name;
   final int recurrence; // in Tagen
+  final String category;
 
-  TaskModel({required this.name, required this.recurrence});
+  TaskModel({required this.name, required this.recurrence, required this.category});
 }
