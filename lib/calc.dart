4a47d42b303dfa62014231693bafac29ff80b5b9
anitagasteiner <anita.gasteiner@posteo.de>
2026-04-28 09:56:04 +0200

    get status out of due date and recurrence

--- lib/calc.dart
+++ lib/calc.dart
@@ -1,4 +1,5 @@
 import './models/task_model.dart';
+import './models/task_state.dart';
 
 String firstName = 'Anita';
 String lastName = 'Gasteiner';
@@ -24,3 +25,31 @@ double getValue(List<TaskModel> tasks, String state) {
   int tasksAmount = tasks.length;
   return 250/tasksAmount*countByState(tasks, state);
 }
+
+TaskState getStatus(DateTime dueDate, int recurrence) {
+  int turningPointDoneRecently = recurrence ~/ 4;
+  int turningPointStillFine = recurrence ~/4*2;
+  int turningPointToDoSoon = recurrence ~/4*3;
+  int turningPointToDo = recurrence;
+  final today = removeTime(DateTime.now());
+  final due = removeTime(dueDate);
+  final difference = due.difference(today).inDays;
+  if (difference < turningPointDoneRecently) {
+    return TaskState.done;
+  }
+  if (difference < turningPointStillFine) {
+    return TaskState.doneRecently;
+  }
+  if (difference < turningPointToDoSoon) {
+    return TaskState.stillFine;
+  }
+  if (difference < turningPointToDo) {
+    return TaskState.toDoSoon;
+  }
+  else {
+    return TaskState.toDo;
+  }
+  
+}
+
+DateTime removeTime(DateTime d) => DateTime(d.year, d.month, d.day);
